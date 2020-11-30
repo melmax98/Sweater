18a454a09a9df4a9a6b71e8b73a06d2477a5ca2e
melmax98 <melmax98@gmail.com>
2020-12-01 00:49:20 +0200

    Add login and registration, SonarLint optimisation, .gitignore

---
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -0,0 +1,52 @@
+package com.example.sweater.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.sweater.domain.Message;
+
+import com.example.sweater.repos.MessageRepo;
+
+@Controller
+public class MainController {
+
+	@Autowired
+	private MessageRepo messageRepo;
+
+	@GetMapping("/")
+	public String greeting (Model model) {
+		return "greeting";
+	}
+
+	@GetMapping("/main")
+	public String main(Model model) {
+		Iterable<Message> messages = messageRepo.findAll();
+		model.addAttribute("messages", messages);
+		return "main";
+	}
+
+	@PostMapping("/main")
+	public String add(@ModelAttribute("message") Message message, Model model) {
+		messageRepo.save(message);
+		return "redirect:main";
+	}
+
+	@PostMapping("filter")
+	public String filter(@RequestParam String filter, Model model) {
+		Iterable<Message> messages = null;
+
+		if (filter != null && !filter.isEmpty()) {
+			messages = messageRepo.findByTag(filter);
+		} else {
+			messages = messageRepo.findAll();
+		}
+		model.addAttribute("messages", messages);
+		return "main";
+	}
+
+}
